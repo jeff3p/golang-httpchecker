15da11052b0fac643de7673970ea4652bdacf4eb
jeff3p <jeff3p@gmail.com>
2026-07-24 16:09:46 -0400

    Refactor main.go to print 'Hello World'
    Removed URL checking logic and added a hello world function.

--- main.go
+++ main.go
@@ -1,36 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"os"
-	"time"
-)
+import "fmt"
+
+func helloworld() string {
+	return "Hello World!!"
+}
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Printf("Usage: %s <url>\n", os.Args[0])
-		os.Exit(1)
-	}
-
-	url := os.Args[1]
-
-	client := &http.Client{
-		Timeout: 10 * time.Second,
-	}
-
-	resp, err := client.Get(url)
-	if err != nil {
-		fmt.Printf("URL is unavailable: %v\n", err)
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode >= 200 && resp.StatusCode < 400 {
-		fmt.Printf("URL is available. Status: %s\n", resp.Status)
-		os.Exit(0)
-	}
-
-	fmt.Printf("URL returned non-success status: %s\n", resp.Status)
-	os.Exit(1)
+	fmt.Println(helloworld())
 }
