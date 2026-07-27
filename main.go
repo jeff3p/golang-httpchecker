374747820275dedea91684e6a5e3b83e80096bb1
jeff3p <jeff3p@gmail.com>
2026-07-27 09:12:24 -0400

    Implement HTTP server to respond with 'Hello, World!'
    Refactored main.go to use net/http package for web server.

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
-
-func helloworld() string {
-	return "Hello World!!"
-}
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println(helloworld())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, World!")
+	})
+
+	fmt.Println("Listening on :8080")
+	http.ListenAndServe(":8080", nil)
 }
